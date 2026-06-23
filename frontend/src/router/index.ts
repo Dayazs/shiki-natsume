f66a0cb41a1ccf3017ea49a54cb878a1dc421401
Dayazs <Dayazs@outlook.com>
2026-06-23 19:12:00 +0800

    feat: 新增导航栏测试路由

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,7 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/topnav', component: () => import('@/components/TopNav.vue') },
+  ],
 })
 
 export default router
