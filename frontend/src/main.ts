6873daabbc08006fe82c25b872915238b409ccf2
Dayazs <Dayazs@outlook.com>
2026-06-23 19:14:19 +0800

    feat: 新增动态更改根字体大小方法

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,21 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// 引入全局样式
+import '@/css/global.scss'
+
+// 实现根据屏幕动态更改根字体大小
+function setRem() {
+  const designWidth = 1920
+  const baseSize = 16
+  const minWidth = 1024
+  let width = Math.max(minWidth, window.innerWidth)
+  document.documentElement.style.fontSize =
+    (width / designWidth) * baseSize + 'px'
+}
+setRem()
+window.addEventListener('resize', setRem)
+
 const app = createApp(App)
 
 app.use(createPinia())
