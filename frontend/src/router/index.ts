43f8908a4ecd1bd761219e4d14609ec2becca323
Dayazs <Dayazs@outlook.com>
2026-07-01 16:28:07 +0800

    feat: 新增路由

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,7 +3,21 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/topnav', component: () => import('@/components/TopNav.vue') },
+    {
+      // 顶部导航栏
+      path: '/topnav',
+      component: () => import('@/components/TopNav/index.vue'),
+    },
+    {
+      // 首页
+      path: '/hero',
+      component: () => import('@/components/Hero/index.vue'),
+    },
+    {
+      // 吧台
+      path: '/bar',
+      component: () => import('@/components/Bar/index.vue'),
+    },
   ],
 })
 
